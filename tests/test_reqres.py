b4f3868cdf42c7fe8573d252c5042a8490e80be5
Irina.Voron <yarutich.irina@gmail.com>
2024-04-18 17:55:53 +0300

    Add get tests

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -1,30 +1,55 @@
 import requests
 from jsonschema import validate
 
-from schemas import list_users_schema
+from schemas import list_users_schema, single_user_schema
 
 url = 'https://reqres.in'
+endpoint_list_users = '/api/users'
+endpoint_single_user = '/api/users/2'
+endpoint_user_not_found = '/api/users/23'
 
 
 def test_list_users_status_code():
-    endpoint = '/api/users'
-    response = requests.get(url + endpoint, {'page': 2})
+    response = requests.get(url + endpoint_list_users, {'page': 2})
 
     assert response.status_code == 200
 
 
 def test_list_users_json_schema():
-    endpoint = '/api/users'
-    response_body = requests.get(url + endpoint, {'page': 2}).json()
+    response_body = requests.get(url + endpoint_list_users, {'page': 2}).json()
 
     validate(response_body, list_users_schema)
 
 
-def test_number_per_page():
-    endpoint = '/api/users'
-    response_body = requests.get(url + endpoint, {'page': 2}).json()
+def test_list_users_number_per_page():
+    response_body = requests.get(url + endpoint_list_users, {'page': 2}).json()
 
     assert response_body['per_page'] == len(response_body['data'])
+
+
+def test_single_user_status_code():
+    response = requests.get(url + endpoint_single_user)
+
+    assert response.status_code == 200
+
+
+def test_single_user_json_schema():
+    response_body = requests.get(url + endpoint_single_user).json()
+
+    validate(response_body, single_user_schema)
+
+
+def test_user_not_found_status_code():
+    response = requests.get(url + endpoint_user_not_found)
+
+    assert response.status_code == 404
+
+
+def test_user_not_found_empty_response_body():
+    response_body = requests.get(url + endpoint_user_not_found).json()
+
+    assert not response_body
+
 
 
 # def test_body():
