4087d9f6d3ce52be2faa90e6ad92d18fe6749bde
Irina.Voron <yarutich.irina@gmail.com>
2024-04-19 18:42:34 +0300

    Delete api_methods.py

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -1,16 +1,34 @@
+import json
+from allure_commons.types import AttachmentType
 from jsonschema import validate
 import schemas
-from utils.api_methods import get_api_request, post_api_request, put_api_request, delete_api_request
+import requests
+import allure
 
+base_url = 'https://reqres.in'
 users = '/api/users'
 single_user = '/api/users/2'
 user_not_found = '/api/users/23'
 register = '/api/register'
 
 
+def api_request(endpoint, method, data=None, params=None):
+    url = base_url + endpoint
+    with allure.step("API request"):
+        response = requests.request(method, url, data=data, params=params)
+        allure.attach(
+            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+            name='Response',
+            attachment_type=AttachmentType.JSON,
+            extension='.json'
+        )
+        return response
+
+
 def test_list_users_status_code_and_schema():
-    response = get_api_request(
+    response = api_request(
         endpoint=users,
+        method='GET',
         params={'page': 2}
     )
     response_body = response.json()
@@ -20,8 +38,9 @@ def test_list_users_status_code_and_schema():
 
 
 def test_list_users_number_per_page():
-    response_body = get_api_request(
+    response_body = api_request(
         endpoint=users,
+        method='GET',
         params={'page': 2}
     ).json()
 
@@ -29,7 +48,10 @@ def test_list_users_number_per_page():
 
 
 def test_single_user_status_code_and_schema():
-    response = get_api_request(endpoint=single_user)
+    response = api_request(
+        endpoint=single_user,
+        method='GET'
+    )
     response_body = response.json()
 
     assert response.status_code == 200
@@ -37,21 +59,28 @@ def test_single_user_status_code_and_schema():
 
 
 def test_user_not_found_status_code():
-    response = get_api_request(endpoint=user_not_found)
+    response = api_request(
+        endpoint=user_not_found,
+        method='GET'
+    )
 
     assert response.status_code == 404
 
 
 def test_user_not_found_empty_response_body():
-    response_body = get_api_request(endpoint=user_not_found).json()
+    response_body = api_request(
+        endpoint=user_not_found,
+        method='GET'
+    ).json()
 
     assert not response_body
 
 
 def test_create_status_code_and_schema():
-    response = post_api_request(
+    response = api_request(
         endpoint=users,
-        payload={'name': 'morpheus', 'job': 'leader'}
+        method='POST',
+        data={'name': 'morpheus', 'job': 'leader'}
     )
     response_body = response.json()
 
@@ -62,9 +91,10 @@ def test_create_status_code_and_schema():
 def test_create_name_and_job_in_response():
     name = 'morpheus'
     job = 'leader'
-    response_body = post_api_request(
+    response_body = api_request(
         endpoint=users,
-        payload={'name': name, 'job': job}
+        method='POST',
+        data={'name': name, 'job': job}
     ).json()
 
     assert response_body['name'] == name
@@ -72,9 +102,10 @@ def test_create_name_and_job_in_response():
 
 
 def test_update_status_code_and_schema():
-    response = put_api_request(
+    response = api_request(
         endpoint=single_user,
-        payload={'name': 'morpheus', 'job': 'zion resident'}
+        method='PUT',
+        data={'name': 'morpheus', 'job': 'zion resident'}
     )
     response_body = response.json()
 
@@ -83,24 +114,28 @@ def test_update_status_code_and_schema():
 
 
 def test_update_no_name_in_response_body():
-    response_body = put_api_request(
+    response_body = api_request(
         endpoint=single_user,
-        payload={'job': 'zion resident'}
+        method='PUT',
+        data={'job': 'zion resident'}
     ).json()
 
     assert 'name' not in response_body
 
 
 def test_delete_status_code():
-    response = delete_api_request(endpoint=single_user)
+    response = api_request(
+        endpoint=single_user,
+        method='DELETE')
 
     assert response.status_code == 204
 
 
 def test_register_successful_status_code_and_schema():
-    response = post_api_request(
+    response = api_request(
         endpoint=register,
-        payload={'email': 'eve.holt@reqres.in', 'password': 'pistol'}
+        method='POST',
+        data={'email': 'eve.holt@reqres.in', 'password': 'pistol'}
     )
     response_body = response.json()
 
@@ -109,9 +144,10 @@ def test_register_successful_status_code_and_schema():
 
 
 def test_register_unsuccessful_status_code_and_schema():
-    response = post_api_request(
+    response = api_request(
         endpoint=register,
-        payload={'email': 'eve.holt@reqres.in'}
+        method='POST',
+        data={'email': 'eve.holt@reqres.in'}
     )
     response_body = response.json()
 
@@ -120,9 +156,10 @@ def test_register_unsuccessful_status_code_and_schema():
 
 
 def test_register_unsuccessful_error_message():
-    response_body = post_api_request(
+    response_body = api_request(
         endpoint=register,
-        payload={'email': 'eve.holt@reqres.in'}
+        method='POST',
+        data={'email': 'eve.holt@reqres.in'}
     ).json()
 
     assert response_body['error'] == 'Missing password'
